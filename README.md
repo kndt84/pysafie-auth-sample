d28ff680e6d841d40d8a1efc88e961b7fd0af0ca
Takashi Kaneda <takashi.kaneda@futurestandard.co.jp>
2021-09-13 06:08:06 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # pysafie-auth-sample
+
+Install pip modules
+```
+pip install -r requirements.txt
+```
+
+Set required information in config.yml
+```
+client_id: 
+client_secret:
+redirect_uri:
+```
+
+Run server
+```
+sudo python3 runserver.py
+```
+
+Access the following url with your browser
+
+https://127.0.0.1/authsample
+
+Input safie ID and password
+
+You can see the device list retrieved from the Safie API
---
+++ runserver.py
@@ -0,0 +1,42 @@
+import yaml
+from bottle import route, run, request, redirect, ServerAdapter
+from gevent.pywsgi import WSGIServer
+import pysafie
+
+CERT = 'cert.pem'
+KEY = 'key.pem'
+
+with open('config.yml', 'r') as yml:
+    conf = yaml.load(yml)
+
+
+class SSLWebServer(ServerAdapter):
+    def run(self, handler):
+        srv = WSGIServer(('127.0.0.1', 443), handler,
+                         certfile=CERT,
+                         keyfile=KEY)
+        srv.serve_forever()
+
+
+@route('/authsample')
+def authsample():
+    return '<a href="https://openapi.safie.link/v1/auth/authorize?client_id=481a6e416310&response_type=code&scope=safie-api&redirect_uri=https://127.0.0.1">click here</a>'
+
+
+@route('/')
+def get_auth_code():
+    code = request.query.code
+    client_id = conf['client_id']
+    client_secret = conf['client_secret']
+    redirect_uri = conf['redirect_uri']
+
+    print(conf)
+    print(code)
+
+    safie = pysafie.Safie(client_id, client_secret, redirect_uri)
+    safie.get_access_token(code)
+    res = safie.get_device_list()
+    return res.json()
+
+
+run(host='127.0.0.1', port=443, server=SSLWebServer)
